efbe7530134510c97a381408c0559c7aadf5f3b3
Jose Orlando Pereira <jop@di.uminho.pt>
2005-10-25 14:25:54 +0000

    daemon thread and cleanup daemon thread and cleanup

--- test/FileOps.java
+++ test/FileOps.java
@@ -1,48 +1,13 @@
 /*
- * NeEM - Network-friendly Epidemic Multicast
- * Copyright (c) 2005, University of Minho
- * All rights reserved.
- *
- * Contributors:
- *  - Pedro Santos <psantos@gmail.com>
- *  - Jose Orlando Pereira <jop@di.uminho.pt>
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- * 
- *  - Redistributions of source code must retain the above copyright
- *  notice, this list of conditions and the following disclaimer.
- * 
- *  - Redistributions in binary form must reproduce the above copyright
- *  notice, this list of conditions and the following disclaimer in the
- *  documentation and/or other materials provided with the distribution.
- * 
- *  - Neither the name of the University of Minho nor the names of its
- *  contributors may be used to endorse or promote products derived from
- *  this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * OutputFile.java
+ * FileOps.java
  *
  * Created on May 27, 2005, 3:09 PM
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ */
+
+/**
+ *  Logging utilities class.
+ * @author psantos@lsd.di.uminho.pt
  */
 
 package test;
@@ -53,12 +18,12 @@ import java.lang.*;
 import java.util.*;
 
 
-/**
- *  Estudo para file output de logs
- * @author psantos
- */
 public abstract class FileOps {
     
+    /** Write a string to a file.
+     * @param o The string to be written.
+     * @param file_name File to write to.
+     */
     public static void write(String o, String file_name) {
         try {
             FileOutputStream fos = new FileOutputStream(file_name, true);
@@ -72,6 +37,10 @@ public abstract class FileOps {
         } catch (IOException ioe) {}
     }
     
+    /** Read a string from a file.
+     * @param file_name File to read from.
+     * @return The string read.
+     */
     public static String read(String file_name) {
         String res = null;
 
