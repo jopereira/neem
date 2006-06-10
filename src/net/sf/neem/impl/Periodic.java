067d0a52fd98e329cb4cc02c7c7a03a7891ad061
Jose Orlando Pereira <jop@di.uminho.pt>
2006-06-10 17:02:41 +0000

    silly bug fixed silly bug fixed

--- src/net/sf/neem/impl/Periodic.java
+++ src/net/sf/neem/impl/Periodic.java
@@ -73,7 +73,7 @@ public abstract class Periodic implements Runnable {
 	
 	public void doIt() {
 		if (running)
-			runnable.run();
+			run();
 		if (running)
 			trans.schedule(runnable, interval);
 	}
